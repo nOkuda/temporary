7bd3c4f11ab6513b39d9422fa1a8625375534f62
Nozomu Okuda <Nozomu.Okuda@gmail.com>
2020-03-19 14:44:05 -0400

    tmp

---
+++ precache-manifest.41297dd380fc98f68eb7193a830f9943.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ec726628d37ec4151bca99da9ec1e6",
+    "url": "/front/index.html"
+  },
+  {
+    "revision": "0e4d31252b5ec7c33478",
+    "url": "/front/static/css/main.25ac248d.chunk.css"
+  },
+  {
+    "revision": "fb17cb90d561eeadbd67",
+    "url": "/front/static/js/2.dc0d8cd9.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/front/static/js/2.dc0d8cd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4d31252b5ec7c33478",
+    "url": "/front/static/js/main.9f134466.chunk.js"
+  },
+  {
+    "revision": "7041ebb2047e7e6eddaa",
+    "url": "/front/static/js/runtime-main.f405f42b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/front/static/media/logo.5d5d9eef.svg"
+  }
+]);
